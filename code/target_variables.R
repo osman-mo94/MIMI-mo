68fcfdf5224c0089740ae8c3590a640d925a26f5
osman-mo94 <osman.mo94@gmail.com>
2023-05-25 17:24:39 +0100

    Add a column to indicate if a food item has been purchased from the marketplace. This is necessary to know if the household has access to the fortification vehicle.

--- code/target_variables.R
+++ code/target_variables.R
@@ -29,38 +29,16 @@ colnames(food_consumption)
 
 # Add a column with name of food item based on "item_cd" for staple grains:
 food_consumption <- food_consumption  %>% mutate(food_item = dplyr::case_when(
-  item_cd == 10 ~ "Guinea corn/sorghum",
-  item_cd == 11 ~ "Millet",
   item_cd == 13 ~ "Rice (local)",
   item_cd == 14 ~ "Rice (imported)",
   item_cd == 16 ~ "Maize flour",
-  item_cd == 17 ~ "Yam flour",
-  item_cd == 18 ~ "Cassava flour",
   item_cd == 19 ~ "Wheat flour",
-  item_cd == 20 ~ "Maize (unshelled/on the cob)",
-  item_cd == 22 ~ "Maize (shelled/off the cob)",
-  item_cd == 23 ~ "Other grains and flour",
   item_cd == 25 ~ "Bread",
   item_cd == 26 ~ "Cake",
   item_cd == 27 ~ "Buns/pofpof/donuts",
   item_cd == 28 ~ "Biscuits",
-  item_cd == 29 ~ "Meat pie/sausage roll",
-  item_cd == 30 ~ "Cassava - roots",
-  item_cd == 31 ~ "Yam - roots",
-  item_cd == 32 ~ "Gari - white",
-  item_cd == 33 ~ "Gari - yellow",
-  item_cd == 34 ~ "Cocoyam",
-  item_cd == 35 ~ "Plantains",
-  item_cd == 36 ~ "Sweet potatoes",
-  item_cd == 37 ~ "Potatoes",
-  item_cd == 38 ~ "Other roots and tuber",
-  item_cd == 40 ~ "Soya beans",
-  item_cd == 41 ~ "Brown beans",
-  item_cd == 42 ~ "White beans",
   TRUE ~ NA_character_
 ))
-
-# Perhaps only need to focus on foods of interest in the command above
 
 # The following column "s06bq01" identifies if the household consumed the food item in the previous 7 days:
 food_consumption$consumed <- ifelse(food_consumption$s06bq01 == 1, "Yes", 
@@ -74,19 +52,30 @@ food_consumption <- food_consumption %>%
          quantity_unit = s06bq02b,
          unit_other = s06bq02b_os,
          quantity_size = s06bq02c,
-         conversion_factor = s06bq02_cvn)
+         conversion_factor = s06bq02_cvn,
+         quantity_purchased = s06bq03)
+
+# Create a column if at least some of the food item was purchased (i.e. quantity > 0, and not NA):
+food_consumption$food_purchased <- ifelse(food_consumption$quantity_purchased > 0, 
+                                          "Yes", "No")
+
+# If NA in food_purchased column, then replace with "No":
+food_consumption$food_purchased <- ifelse(is.na(food_consumption$food_purchased), 
+                                          "No", food_consumption$food_purchased)
+
 
 #-------------------------------------------------------------------------------
 
 # Create df for target variables, for each household ID: 
 target_variables <- cover %>% dplyr::select("hhid")
 
-# Add-in columns for target variable (staple grains) - binary yes/no depending on if they have been consumed:
+# Add-in columns for target variable (staple grains) - binary yes/no depending on if they have been consumed and purchased:
 target_variables <- target_variables %>%
   # Rice (local):
   left_join(food_consumption %>% 
               filter(food_item == "Rice (local)",
-                     consumed == "Yes") %>% 
+                     consumed == "Yes",
+                     food_purchased == "Yes") %>% 
               # Create a column in analysis_df for these individuals
               dplyr::select("hhid", "consumed") %>%
               rename("rice_local" = "consumed"),
@@ -94,7 +83,8 @@ target_variables <- target_variables %>%
   # Rice (imported):
   left_join(food_consumption %>% 
               filter(food_item == "Rice (imported)",
-                     consumed == "Yes") %>% 
+                     consumed == "Yes",
+                     food_purchased == "Yes") %>% 
               # Create a column in analysis_df for these individuals
               dplyr::select("hhid", "consumed") %>%
               rename("rice_imported" = "consumed"),
@@ -102,7 +92,8 @@ target_variables <- target_variables %>%
   # Maize flour: 
   left_join(food_consumption %>% 
               filter(food_item == "Maize flour",
-                     consumed == "Yes") %>% 
+                     consumed == "Yes",
+                     food_purchased == "Yes") %>% 
               # Create a column in analysis_df for these individuals
               dplyr::select("hhid", "consumed") %>%
               rename("maize_flour" = "consumed"),
@@ -110,11 +101,15 @@ target_variables <- target_variables %>%
   # Wheat flour: 
   left_join(food_consumption %>% 
               filter(food_item == "Wheat flour",
-                     consumed == "Yes") %>% 
+                     consumed == "Yes",
+                     food_purchased == "Yes") %>% 
               # Create a column in analysis_df for these individuals
               dplyr::select("hhid", "consumed") %>%
               rename("wheat_flour" = "consumed"),
             by = "hhid") %>% 
+
+# Baked goods do not necessarily need to have been purchased, as they could have been made using purchased flour:    
+
   # Bread: 
   left_join(food_consumption %>% 
               filter(food_item == "Bread",
@@ -225,9 +220,9 @@ colour_palette <- ghibli_palette("PonyoMedium", n = 4)
 ggplot(grain_consumption, aes(x = reorder(grain, -percentage_consumed), 
                               y = percentage_consumed, fill = grain)) + 
   geom_col(show.legend = F) + labs(x = "Grain", y = "% of Households") +
+  ggtitle("Percentage of households with access to each fortification vehicle in Nigeria") +
   scale_fill_manual(values = colour_palette) + theme_pander()
 
-# Remove objects that are not required further: 
-rm(list = c("cover", "food_consumption", "grain_consumption", "maizef_consumption", 
-            "riceimp_consumption", "riceloc_consumption", "wheatf_consumption",
-            "colour_palette", "grain", "percentage_consumed"))
+# Save the plot:
+# ggsave("figures/grain_consumption.png", width = 10, height = 6, dpi = 800)
+
